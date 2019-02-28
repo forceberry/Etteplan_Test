46780921551ff350a9acba63d6c08dde1b6c65db
forceberry <otto.forsberg@gmail.com>
2019-02-28 10:55:09 +0200

    Add project files.

---
+++ EtteplanBackend/Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EtteplanBackend.Models
+{
+    public class Car
+    {
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string RegisterId { get; set; }
+        public int Year { get; set; }
+        public DateTime InspectionDate { get; set; }
+        public double EngineDisplacement { get; set; }
+        public int EnginePower { get; set; }
+
+    }
+}
